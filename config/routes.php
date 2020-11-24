d41b0498b6893257c95c01aff0e89a8228040d4b
yinjiayao <yinjiayao>
2020-11-24 14:35:51 +0800

    first commit

---
+++ config/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\HttpServer\Router\Router;
+
+Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+
+Router::get('/favicon.ico', function () {
+    return '';
+});
+
+Router::get('/get', 'App\Controller\IndexController@get');
+Router::post('/post', 'App\Controller\IndexController@post');
+
+Router::addRoute(['GET', 'POST', 'HEAD'], '/multi', 'App\Controller\IndexController::multi');
+
